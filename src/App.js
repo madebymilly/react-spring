b677aec5a453a04a67d272932c35edad64ca7b3c
Milly Uylenbroek <m.uylenbroek@eagerly.nl>
2022-03-09 14:03:56 +0100

    added first animation & changed some styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
+import React from 'react'
+import { useSpring, animated } from 'react-spring'
 
 function App() {
+
+  // create animation with useSpring and callback function:
+  const [props] = useSpring(
+    () => ({ width: 100, from: { width: 0}, config: {duration: 3000} })
+    //() => ({ width: 100, from: { width: 0} })
+  );
+
   return (
     <div className="App">
       <main>
         <div className="Physics">
           <div className="Physics__A">A</div>
-          <div className="Physics__divider"></div>
+          <animated.div 
+            className="Physics__divider"
+            style={{width: props.width.to(w => `${w}%`)}}
+          ></animated.div>
           <div className="Physics__B">B</div>
         </div>
         <button>STOP</button>
