eea4c4e1812097de9c55c2ee6387d4570763f91e
Milly Uylenbroek <m.uylenbroek@eagerly.nl>
2022-03-09 14:20:48 +0100

    move first example to sep component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import './App.css';
-import React from 'react'
-import { useSpring, animated } from 'react-spring'
+
+import Physics from './Physics';
 
 function App() {
-
-  // create animation with useSpring and callback function:
-  const [props] = useSpring(
-    () => ({ width: 100, from: { width: 0}, config: {duration: 3000} })
-    //() => ({ width: 100, from: { width: 0} })
-  );
-
   return (
     <div className="App">
       <main>
-        <div className="Physics">
-          <div className="Physics__A">A</div>
-          <animated.div 
-            className="Physics__divider"
-            style={{width: props.width.to(w => `${w}%`)}}
-          ></animated.div>
-          <div className="Physics__B">B</div>
-        </div>
-        <button>STOP</button>
+        <Physics />
       </main>
 
     </div>
