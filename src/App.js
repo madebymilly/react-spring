bf1ce957db88a480ecc6f7f7e5952ec35ac05326
Milly Uylenbroek <m.uylenbroek@eagerly.nl>
2022-03-09 13:41:44 +0100

    first set up with create react app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <div className="Physics">
+          <div className="Physics__A">A</div>
+          <div className="Physics__divider"></div>
+          <div className="Physics__B">B</div>
+        </div>
+        <button>STOP</button>
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
