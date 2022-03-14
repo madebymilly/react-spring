5469acebb25724c3eec77ab7e4bae9541a5d2032
Milly Uylenbroek <m.uylenbroek@eagerly.nl>
2022-03-14 13:55:11 +0100

    add animations to slider

--- src/Slider.js
+++ src/Slider.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { useInterval } from 'react-use';
+import { useInterval } from 'react-use'
+import { useTransition, animated } from 'react-spring'
 
 import './Slider.css'
 
@@ -31,7 +32,23 @@ export default function Slider() {
   const item = slides[index];
   const increment = () => setIndex(state => (state + 1) % slides.length);
   useInterval(increment, 2000);
+
+  const transitions = useTransition( //(activeSlide, unique key for each slide, config)
+    slides[index],
+    {
+      // keys: item => item.id,
+      key: item.id,
+      from: { opacity: 0, transform: 'scale(1.1)'}, 
+      enter: { opacity: 1, transform: 'scale(1)'}, 
+      leave: { opacity: 0, transform: 'scale(0.9)'}
+    }
+  );
+
   return (
-    <div className="Slider" key={index} style={{backgroundImage: `url(${item.url})`}}></div>
+    <div className="Slider">
+      {transitions((props, item) => (
+        <animated.div className="Slider__item" key={props.key} style={{backgroundImage: `url(${item.url})`, ...props}}></animated.div>
+      ))}
+    </div>
   )
 }
